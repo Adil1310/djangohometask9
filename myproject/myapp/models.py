67489704d684f43a28e015a494961ba7f6aaf132
Adil Rahimov <adilrahimov1310@gmail.com>
2023-07-27 23:13:18 +0600

    hometask added

---
+++ myproject/myapp/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Myusers(models.Model):
+    fname = models.CharField(max_length=50)
+    lname = models.CharField(max_length=50)
+    birthyear = models.IntegerField()
+    state = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f'{self.fname} {self.lname} {self.birthyear} {self.state}'
